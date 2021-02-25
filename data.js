d4499eac7fad17c0b2b1ca4ecc2240c4de289d66
Conan Browne <Ventrik101@Gmail.com>
2021-02-25 17:41:34 -0500

    Fixed data.js

--- data.js
+++ data.js
@@ -1,30 +1,30 @@
 const steak = [
-  'season steak generously with salt, pepper and garlic powder',
-  'place in zip lock bag',
-  'cook in sous vide at 120 F for 1-2 hours',
-  'remove from bag and pat dry',
-  'heat pan with grapeseed oil and a quarter stick of butter',
-  'cook steak for 30-60 seconds per side using a spoon to baste with butter',
-  'rest for 10 minutes',
-  'enjoy'
-];
+	'Season a 2 inch thick steak liberally with salt, freshly ground black pepper, and garlic powder.',
+	'Vacuum seal seasoned steak.',
+	'Sous vide at 131 degrees F for 3 and a half hours.',
+	'Remove from the bag, reserve juices if any, and pat steak dry.',
+	'Get a pan hot, add grapeseed oil, half stick of butter, sprig of thyme, and few cloves of crushed garlic with the steak to the pan.',
+	'Sear the steak for 30-60 seconds per side and using a spoon to baste with butter.',
+	'Set steak to rest for 10 minutes.',
+	'Serve and enjoy!',
+]
 
 const brusselSprouts = [
-  'wash brussel sprouts',
-  'cut off base and chop in half',
-  'toss in bowl with olive oil, balsamic vinegar and salt',
-  'preheat oven to 500 F',
-  'coat baking sheet with olive oil',
-  'place in flat side down',
-  'cook for 20 minutes',
-  'place back in bowl and add salt and pepper',
-  'enjoy'
-];
+	'Heat your oven to 500 degrees F.',
+	'Rinse and trim bottoms off brussel sprouts.',
+	'Cut the sprouts in half ',
+	'Toss sprouts in bowl with olive oil, balsamic vinegar, and salt.',
+	'Get a rimmed baking sheet and coat with olive oil',
+	'Place sprouts cut side down on baking sheet',
+	'Roast in the oven for 20 minutes or until as charred as you prefer',
+	'Serve sprouts in a bowl and check for seasoning, add salt + pepper if needed.',
+	'Enjoy!',
+]
 
 const mashPotatoes = [
-  'boil water',
-  'tear open bag of of instant potato mix and pour into bowl',
-  'pour in water',
-  'mix',
-  'enjoy'
-];
+	'Bring a pot of water to a boil.',
+	'Open one bag of of instant potato mix and pour into a hear proof bowl.',
+	'Add the boiling water to the instant potato mix.',
+	'Mix adding in butter and milk as desired',
+	'Serve and enjoy!',
+]
